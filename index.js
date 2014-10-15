35ace7e983995da6ac9532f377e432c8a9c9ce3e
Javier Carrillo <stringparser@gmail.com>
2014-10-16 01:31:52 +0200

    do things only at the moment they are needed

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ var callersPath = require('callers-path');
 
 function requirem(pathName, _opts){
   var error, opts = type(_opts || pathName).plainObject || { };
-  opts.origin = type(opts.origin).function || requirem;
-  opts.pattern = type(opts.pattern || _opts || pathName).regexp || /\.(js)$/i;
   opts.pathName = (type(pathName).string || '').replace(/^\./, function(){
-      return path.dirname(
-        path.join(callersPath(opts.origin), type(opts.dirName || _opts).string || '')
-      );
+      opts.origin = type(opts.origin).function || requirem;
+      opts.dirName = type(opts.dirName || _opts).string || '';
+      return path.dirname( path.join(callersPath(opts.origin), opts.dirName) );
     });
 
   var camelName, fileExports = { };
   try {
     opts.dirls = type(opts.dirls).array || fs.readdirSync(opts.pathName);
     // opts.pathName was a directory
+    opts.pattern = type(opts.pattern || _opts || pathName).regexp || /\.(js)$/i;
     opts.dirls.forEach(function (fileName){
       if( !opts.pattern.test(fileName) ){ return ; }
       fileName = path.resolve(opts.pathName, fileName);
