65d9fb6559f80efb2eb7ad06b8d8b5f90cbd7913
Javier Carrillo <stringparser@gmail.com>
2014-10-16 00:49:55 +0200

    more tests

--- index.js
+++ index.js
@@ -6,47 +6,43 @@ var type = require('utils-type');
 var camelcase = require('camelcase');
 var callersPath = require('callers-path');
 
-function requirem( dirName, pathName, opts){
-  var opts = type(opts || pathName || dirName).plainObject || { };
-  opts.origin = type(opts.origin).function;
-  dirName = type(dirName).string || path.dirname(
-    callersPath(opts.origin || requirem) );
 
-  var parsed = type(pathName).string
-    ? path.resolve(dirName, path.basename(pathName)) : dirName;
-  //
-  // isModule?
-  // <^> a file or something that resolves to one
-  var resolveError = null;
+function requirem(pathName, _opts){
+  var error, opts = type(_opts || pathName).plainObject || { };
+  opts.origin = type(opts.origin).function || requirem;
+  opts.pattern = type(opts.pattern || _opts || pathName).regexp || /\.(js)$/i;
+  opts.pathName = (type(pathName).string || '').replace(/^\./, function(){
+      return path.dirname(callersPath(opts.origin));
+    });
+
+  var fileExports = { };
   try {
-    parsed = require.resolve(parsed);
-    if( opts.reload ){ delete require.cache[parsed]; }
-    return require(parsed);
-  } catch(err) { resolveError = err; }
+    opts.dirls = type(opts.dirls).array || fs.readdirSync(opts.pathName);
+    // opts.pathName was a directory
+    opts.dirls.forEach(function (fileName){
+      if( !opts.pattern.test(fileName) ){ return ; }
+      fileName = path.resolve(opts.pathName, fileName);
+      var name = camelcase(path.basename(fileName, path.extname(fileName)));
+      // camelcase'em & is no need for fileExports['some-key']
+      if( opts.reload ){ delete require.cache[fileName]; }
+      fileExports[name] = require(fileName);
+    });
+    opts = error = null; // wipe
+    return fileExports;
+  } catch(err){ error = err; }
   //
-  // plain directories left
-  // ~
-  var dirls = null, dirError = null;
-  try { dirls = fs.readdirSync(parsed); }
-    catch(err){ dirError = err; }
-  if( dirError ){ throw (resolveError || dirError); }
-  //
-  // path is dir and exists
-  // ~
-  dirName = parsed;
-  var camelName = null, fileExports = { };
-  opts.pattern = type(pathName || dirName).regexp || /\.(js)$/i;
-  dirls = dirls.filter(function (fileName){
-    if( !opts.pattern.test(fileName) ){ return ; }
-    parsed = path.resolve(dirName, fileName);
-    camelName = camelcase(path.basename(fileName, path.extname(fileName)));
-    // camelcase'em & is no need for fileExports['some-key']
-    if( opts.reload ){ delete require.cache[parsed]; }
-    try { fileExports[camelName] = require(parsed); }
-      catch(err){ throw err; }
-    return true;
-  });
-
-  return fileExports;
+  // opts.pathName should be a module
+  // <^> meaning: file
+  try {
+    opts.pathName = require.resolve(opts.pathName);
+    opts.loaded = !opts.reload || delete require.cache[opts.pathName];
+    return require(opts.pathName);
+  } catch(err) {
+     fs.exists(opts.pathName, function(exists){
+       if( exists ){ throw error; }
+       else { throw err; }
+     });
+  }
+  // directory?
 }
 module.exports = requirem;
