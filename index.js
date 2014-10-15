4eca09afe7d9170ddbca271fdaa30086fcb75b28
Javier Carrillo <stringparser@gmail.com>
2014-10-16 01:17:24 +0200

    dirrname option added

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ function requirem(pathName, _opts){
   var error, opts = type(_opts || pathName).plainObject || { };
   opts.origin = type(opts.origin).function || requirem;
   opts.pattern = type(opts.pattern || _opts || pathName).regexp || /\.(js)$/i;
+  opts.dirName = type(opts.dirName || _opts).string || '';
   opts.pathName = (type(pathName).string || '').replace(/^\./, function(){
-      return path.dirname(callersPath(opts.origin));
+      return path.dirname(path.join(callersPath(opts.origin), opts.dirName));
     });
 
   var fileExports = { };
