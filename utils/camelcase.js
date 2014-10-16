c3dc0d9cf0ea598d0c1d9e531587807d3ef07365
Javier Carrillo <stringparser@gmail.com>
2014-10-16 21:00:28 +0200

    no smart ass camelCasing, just simple

--- utils/camelcase.js
+++ utils/camelcase.js
@@ -8,8 +8,8 @@ module.exports = function (str) {
 
   return str
   .replace(/^[_.\- ]+/, '')
-    .replace(/(\w+)(?=[_.\- ]+)|(\w+)(?=[A-Z])/, function(m, p1, p2){
-      return (p2 || p1).toLowerCase();
+    .replace(/(\w+)(?=[_.\- ]+)/, function(m, p1){
+      return p1.toLowerCase();
     }).replace(/[_.\- ]+(\w|$)/g, function (m, p1) {
       return p1.toUpperCase();
     });
